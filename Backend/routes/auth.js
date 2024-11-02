41ac6155b8685ff3ae2c7b0069b716574e0cbbca
Piyush Yadav <piyushkrishna11@gmail.com>
2024-11-02 23:17:48 +0530

    Update auth.js

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const passport = require("passport");
 
 const CLIENT_URL = "http://localhost:3000/";
+// cat on run
 
 // Successful login
 router.get("/login/success", (req, res) => {
